cac98c9f03f7be510e53359be7d4a0162559d32d
Rasel9360 <raselmahamud9360@gmail.com>
2024-04-22 23:10:29 +0600

    porject update

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("users");
 
     // get data
     app.get('/coffee', async (req, res) => {
@@ -78,6 +79,46 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
       res.send(result)
+    })
+
+    // user related API
+
+    // get
+    app.get('/user', async (req, res) => {
+      const cursor = userCollection.find();
+      const user = await cursor.toArray();
+      res.send(user);
+    })
+
+    // create
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+    //  update
+    app.patch('/user', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const filter = { email: user.email }
+      const updateUser = {
+        $set: {
+          lastSign: user.lastSign
+        }
+      }
+      const  option = { upsert: true }
+      const result = await userCollection.updateOne(filter, updateUser, option)
+      res.send(result)
+    })
+
+    // Delete
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = userCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
